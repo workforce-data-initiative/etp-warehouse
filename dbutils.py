7bdcee9b7508e2ee426ed93581fa10578d929f65
akebirungi <aretha.kebirungi@sv.cmu.edu>
2017-06-07 21:47:03 +0300

    refactored connection factories, added utils to read db conf from yaml file and validate

--- dbutils.py
+++ dbutils.py
@@ -1,41 +1,72 @@
 import logging
+import yaml
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
 logger = logging.getLogger(__name__)
 
 
-class DbConnectionFactory(object):
+def get_db_conf(conf_file, db_adapter):
 
-    def build_conn_str(self, db_conf):
-        return None
+    conf = None
 
-    def start_session(self, db_conf):
-        return None
+    try:
+        with open(conf_file, 'r') as yml_conf:
+            conf = yaml.load(yml_conf)
+    except FileNotFoundError as err:
+        logger.debug(err)
+
+    for db_conf in conf:
+        if db_conf == db_adapter:
+            return db_conf
 
 
-class PsqlDbConnectionFactory(DbConnectionFactory):
-    db_type = "POSTGRESQL"
+class DbConfValidator(object):
 
-    def build_conn_str(self, db_conf):
+    @staticmethod
+    def validate_conf(db_conf):
+        pass
+
+
+class PsqlDbConfValidator(DbConfValidator):
+
+    @staticmethod
+    def validate_conf(db_conf):
+        logger.info("Validating database connection configs")
+
         if db_conf is None:
-            raise ValueError("Database connection info required")
+            raise ValueError("Database connection configs required")
 
-        return "postgresql://{0}:{1}@{2}:{3}/{4}".format(db_conf['username'],
-                                                         db_conf['password'],
-                                                         db_conf['hostname'],
-                                                         db_conf['port'],
-                                                         db_conf['dbname'])
 
-    def start_session(self, db_conf):
-        logger.info("Starting connection session to database, type {0}".format(self.db_type))
+class DbConnectionUri(object):
 
-        engine = create_engine(self.build_conn_str(db_conf))
-        sessionmaker().configure(bind=engine)
-        return Session()
+    @classmethod
+    def build_conn_uri(cls, db_conf):
+        pass
+
+
+class PsqlDbConnectionUri(DbConnectionUri):
+
+    db_adapter = "postgresql"
+    conn_uri = "{db_adapter}://{username}:{password}@{host}:{port}/{database}"
+
+    @classmethod
+    def build_conn_uri(cls, db_conf):
+        db_conf = PsqlDbConfValidator.validate_conf(db_conf)
+
+        return cls.conn_uri.format(db_adapter=cls.db_adapter, username=db_conf['username'],
+                                    password=db_conf['password'], host=db_conf['host'],
+                                    port=db_conf['port'], database=db_conf['database'])
 
 
 class DbConnection(object):
+    Session = sessionmaker()
 
-    def __init__(self, db_conn, db_info):
-        self.conn_session = db_conn.start_session(db_info)
+    def create_session(self, conn_uri):
+        logger.info("Creating connection session to database")
+
+        engine = create_engine(conn_uri)
+        self.Session.configure(bind=engine)
+        return Session()
+
+
