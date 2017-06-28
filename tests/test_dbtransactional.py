888731428783e6db1a9dd2cd00f99b1cbb59a5e9
akebirungi <aretha.kebirungi@sv.cmu.edu>
2017-06-28 17:34:27 +0300

    update tests and circleci to use postgres

--- tests/test_dbtransactional.py
+++ tests/test_dbtransactional.py
@@ -6,9 +6,6 @@ Tests for transactional database operations
 def test_db_tables_created(db_inspector, db_tables, alembic_tables):
     # inspect db created for tables
     table_list = db_inspector.get_table_names()
-    print(table_list)
-    print(db_tables)
-
     assert table_list == alembic_tables + db_tables
 
 
