a39074c1e874118041cc06ccaff044e5e0d50c5b
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-07-14 19:32:33 +0300

    [#147332929] refactor plan.sh to include db user and version configurations, add correct source link

--- habitat/pgsqldb-server/plan.sh
+++ habitat/pgsqldb-server/plan.sh
@@ -3,35 +3,25 @@
 pkg_name=pgsqldb-server
 pkg_origin=brighthive
 pkg_version=0.1.0
+db_version="9.6.3"
+pkg_svc_user = "tpotdb"
 pkg_maintainer="jee@brighthive.io stanley@brighthive.io aretha@brighthive.io"
 pkg_license=('Apache-2.0')
 pkg_description="PostgreSQL database service for TPOT transactional database."
 pkg_upstream_url="https://github.com/workforce-data-initiative/tpot-warehouse"
-
 # will this syntax pick db.ver from default.toml?
-pkg_source="https://www.postgresql.org/ftp/source/v{{cfg.db.ver}}/postgresql-{{cfg.db.ver}}.tar.gz"
-
-pkg_filename="postgresql-{{cfg.db.ver}}.tar.gz"
+pkg_source="https://ftp.postgresql.org/pub/source/v$db_version/postgresql-$db_version.tar.gz"
+pkg_filename="postgresql-${db_version}.tar.gz"
 pkg_shasum="df088372230b1dd21d87bb81686471508f4c42094d4f4f32b5d8e686fea69fa6"
-pkg_deps=(
-    core/glibc
-    core/openssl
-    core/python
-    core/virtualenv
-    )
-pkg_build_deps=(
-    core/make
-    core/gcc
-    core/readline
-    core/zlib
-    )
+pkg_build_deps=(core/virtualenv core/glibc core/gcc core/make core/gcc-libs)
+pkg_deps=(core/python core/coreutils core/openssl)
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 pkg_bin_dirs=(bin)
 pkg_exports=(
     [host]=db.host
     [port]=db.port
-    [ssl-port]=db.ssl.port
+    [ssl-port]=ssl.port
     )
 pkg_exposes=(port ssl-port)
 
@@ -51,27 +41,7 @@ pkg_exposes=(port ssl-port)
 #   [storage]="port host"
 # )
 
-pkg_interpreters=(
-    bin/bash
-    )
-pkg_svc_user=db.user
-pkg_svc_group="$pkg_svc_user"
-
-
-do_begin() {
-  return 0
-}
-
-# The default implementation is that the software specified in $pkg_source is
-# downloaded, checksum-verified, and placed in $HAB_CACHE_SRC_PATH/$pkgfilename,
-# which resolves to a path like /hab/cache/src/filename.tar.gz. You should
-# override this behavior if you need to change how your binary source is
-# downloaded, if you are not downloading any source code at all, or if your are
-# cloning from git. If you do clone a repo from git, you must override
-# do_verify() to return 0.
-do_download() {
-  do_default_download
-}
+pkg_interpreters=(bin/bash)
 
 # The default implementation tries to verify the checksum specified in the plan
 # against the computed checksum after downloading the source tarball to disk.
@@ -110,24 +80,23 @@ do_prepare() {
 
 # Build postgresql with PL/Python server-side language
 do_build() {
-  cd "${HAB_CACHE_SRC_PATH}/postgresql-{{cfg.db.ver}}"
-  ./configure --with-python --with-openssl && make
+  cd "${HAB_CACHE_SRC_PATH}/postgresql-${db_version}"
+  ./configure --with-python --with-openssl --without-readline --without-zlib && make
 }
 
 # Run postgresql regression suite
 do_check() {
-  cd "${HAB_CACHE_SRC_PATH}/postgresql-{{cfg.db.ver}}"
+  cd "${HAB_CACHE_SRC_PATH}/postgresql-${db_version}"
   make check
 }
 
 # Manually installing postgresql since habitat core
 # postgres is not yet fully supported for cluster mode
 do_install() {
-    cd "${HAB_CACHE_SRC_PATH}/postgresql-{{cfg.db.ver}}"
+    cd "${HAB_CACHE_SRC_PATH}/postgresql-${db_version}"
     make install
     # TODO: -explicitly set LD_LIBRARY_PATH and PATH for cross-platform compatibility
     #       -enable TCP/IP communication
-
 }
 
 # The default implementation is to strip any binaries in $pkg_prefix of their
@@ -138,11 +107,3 @@ do_install() {
 do_strip() {
   return 0
 }
-
-# There is no default implementation of this callback. This is called after the
-# package has been built and installed. You can use this callback to remove any
-# temporary files or perform other post-install clean-up actions.
-do_end() {
-  return 0
-}
-
