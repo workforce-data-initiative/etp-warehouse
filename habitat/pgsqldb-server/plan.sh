175e2025df91fdd0bb58b5a9966ecdb300e384e8
Aretha Kebirungi <aretha.kebirungi@andela.com>
2017-07-27 20:38:19 +0300

    [#147332929] Exclude unused libs and callbacks in habitat plan, add helpers.sh for user/datadir operations, update init hook

--- habitat/pgsqldb-server/plan.sh
+++ habitat/pgsqldb-server/plan.sh
@@ -3,31 +3,28 @@
 pkg_name=pgsqldb-server
 pkg_origin=brighthive
 pkg_version=9.6.3
-pkg_svc_user=tpotdb
-pkg_svc_group=tpotdb
-pkg_svc_data_path=/usr/local/pgsql/data
+pkg_svc_data_path=/var/lib/pgsql/data
 pkg_maintainer="jee@brighthive.io stanley@brighthive.io aretha@brighthive.io"
 pkg_license=('Apache-2.0')
 pkg_description="PostgreSQL database service for TPOT transactional database."
 pkg_upstream_url=https://www.postgresql.org
 pkg_source=https://ftp.postgresql.org/pub/source/v${pkg_version}/postgresql-${pkg_version}.tar.gz
-pkg_filename=postgresql-${pkg_version}.tar.gz
 pkg_shasum=df088372230b1dd21d87bb81686471508f4c42094d4f4f32b5d8e686fea69fa6
-pkg_build_deps=(
-  core/coreutils
-  core/virtualenv
-  core/gcc
-  core/gcc-libs
-  core/make)
+pkg_dirname=postgresql-${pkg_version}
 pkg_deps=(
-  core/bash
-  core/envdir
-  core/glibc
-  core/openssl
-  core/perl
-  core/readline
-  core/zlib
-  core/libossp-uuid
+    core/bash
+    core/python
+    core/glibc
+    core/libossp-uuid
+    core/shadow    
+    core/readline
+    core/zlib
+    core/openssl 
+  )
+pkg_build_deps=(
+    core/coreutils
+    core/gcc
+    core/make
   )
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
@@ -39,38 +36,29 @@ pkg_exports=(
     )
 pkg_exposes=(port ssl-port)
 pkg_interpreters=(bin/bash)
+pkg_svc_user=root
+pkg_svc_group=root
 
-do_clean() {
-  do_default_clean
-}
-
-do_prepare() {
-  build_line "Creating postgres data directories for the postgres service ..."
-  mkdir -pv $pkg_svc_path/pgsql/data/
-}
 
 # Build postgresql with PL/Python server-side language
 do_build() {
-  cd "${HAB_CACHE_SRC_PATH}/postgresql-${pkg_version}"
-  ./configure --disable-rpath \
-              --with-openssl \
-              --prefix="$pkg_prefix" \
-              --with-uuid=ossp \
-              --with-includes="$LD_INCLUDE_PATH" \
-              --with-libraries="$LD_LIBRARY_PATH" \
-              --sysconfdir="$pkg_svc_config_path" \
-              --localstatedir="$pkg_svc_var_path"
-  make
+    cd "${HAB_CACHE_SRC_PATH}/${pkg_dirname}"
+    ./configure --with-python \
+	            --disable-rpath \
+                --with-openssl \
+                --with-uuid=ossp \
+                --with-includes="$LD_INCLUDE_PATH" \
+                --with-libraries="$LD_LIBRARY_PATH" \
+                --sysconfdir="$pkg_svc_config_path" \
+                --localstatedir="$pkg_svc_var_path"
+    make
+
 }
 
-# Manually installing postgresql since habitat core
-# postgres is not yet fully supported for cluster mode
+# Install built postgresql
 do_install() {
-  cd "${HAB_CACHE_SRC_PATH}/postgresql-${pkg_version}"
+  cd "${HAB_CACHE_SRC_PATH}/${pkg_dirname}"
   make install
 }
 
-do_end() {
-  build_line "Symbolic linking all required files ..."
-  ln -fs {{pkg.path}}/* {{pkg.svc_var_path}}
-}
+
